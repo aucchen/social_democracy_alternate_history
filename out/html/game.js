0ddc299f52d7137121646994283b6da15619ed8d
aucchen <cchen.intfic@gmail.com>
2024-04-01 19:08:16 -0700

    spring thing day0 release version

---
+++ out/html/game.js
@@ -0,0 +1,172 @@
+(function() {
+  var game;
+  var ui;
+
+  var DateOptions = {hour: 'numeric',
+                 minute: 'numeric',
+                 second: 'numeric',
+                 year: 'numeric',
+                 month: 'short',
+                 day: 'numeric' };
+
+  var main = function(dendryUI) {
+    ui = dendryUI;
+    game = ui.game;
+
+    // Add your custom code here.
+  };
+
+  var TITLE = "Social Democracy: An Alternate History" + '_' + "Autumn Chen";
+
+  window.showStats = function() {
+    if (window.dendryUI.dendryEngine.state.sceneId.startsWith('library')) {
+        window.dendryUI.dendryEngine.goToScene('backSpecialScene');
+    } else {
+        window.dendryUI.dendryEngine.goToScene('library');
+    }
+  };
+  
+  window.showOptions = function() {
+      var save_element = document.getElementById('options');
+      window.populateOptions();
+      save_element.style.display = "block";
+      if (!save_element.onclick) {
+          save_element.onclick = function(evt) {
+              var target = evt.target;
+              var save_element = document.getElementById('options');
+              if (target == save_element) {
+                  window.hideOptions();
+              }
+          };
+      }
+  };
+
+  window.hideOptions = function() {
+      var save_element = document.getElementById('options');
+      save_element.style.display = "none";
+  };
+
+  window.disableBg = function() {
+      window.dendryUI.disable_bg = true;
+      document.body.style.backgroundImage = 'none';
+      window.dendryUI.saveSettings();
+  };
+
+  window.enableBg = function() {
+      window.dendryUI.disable_bg = false;
+      window.dendryUI.setBg(window.dendryUI.dendryEngine.state.bg);
+      window.dendryUI.saveSettings();
+  };
+
+  window.disableAnimate = function() {
+      window.dendryUI.animate = false;
+      window.dendryUI.saveSettings();
+  };
+
+  window.enableAnimate = function() {
+      window.dendryUI.animate = true;
+      window.dendryUI.saveSettings();
+  };
+
+  window.disableAnimateBg = function() {
+      window.dendryUI.animate_bg = false;
+      window.dendryUI.saveSettings();
+  };
+
+  window.enableAnimateBg = function() {
+      window.dendryUI.animate_bg = true;
+      window.dendryUI.saveSettings();
+  };
+
+  // populates the checkboxes in the options view
+  window.populateOptions = function() {
+    var disable_bg = window.dendryUI.disable_bg;
+    var animate = window.dendryUI.animate;
+    var animate_bg = window.dendryUI.animate_bg;
+    if (disable_bg) {
+        $('#backgrounds_no')[0].checked = true;
+    } else {
+        $('#backgrounds_yes')[0].checked = true;
+    }
+    if (animate) {
+        $('#animate_yes')[0].checked = true;
+    } else {
+        $('#animate_no')[0].checked = true;
+    }
+    if (animate_bg) {
+        $('#animate_bg_yes')[0].checked = true;
+    } else {
+        $('#animate_bg_no')[0].checked = true;
+    }
+  };
+  
+  // This function allows you to modify the text before it's displayed.
+  // E.g. wrapping chat-like messages in spans.
+  window.displayText = function(text) {
+      return text;
+  };
+
+  // This function allows you to do something in response to signals.
+  window.handleSignal = function(signal, event, scene_id) {
+  };
+  
+  // This function runs on a new page. Right now, this auto-saves.
+  window.onNewPage = function() {
+    var scene = window.dendryUI.dendryEngine.state.sceneId;
+    if (scene != 'root') {
+        window.dendryUI.autosave();
+    }
+  };
+
+  window.updateSidebar = function() {
+      $('#qualities').empty();
+      var scene = dendryUI.game.scenes.status;
+      var displayContent = dendryUI.dendryEngine._makeDisplayContent(scene.content, true);
+      $('#qualities').append(dendryUI.contentToHTML.convert(displayContent));
+  };
+
+  window.onDisplayContent = function() {
+      window.updateSidebar();
+  };
+
+  /*
+   * This function copied from the code for Infinite Space Battle Simulator
+   *
+   * quality - a number between max and min
+   * qualityName - the name of the quality
+   * max and min - numbers
+   * colors - if true/1, will use some color scheme - green to yellow to red for high to low
+   * */
+  window.generateBar = function(quality, qualityName, max, min, colors) {
+      var bar = document.createElement('div');
+      bar.className = 'bar';
+      var value = document.createElement('div');
+      value.className = 'barValue';
+      var width = (quality - min)/(max - min);
+      if (width > 1) {
+          width = 1;
+      } else if (width < 0) {
+          width = 0;
+      }
+      value.style.width = Math.round(width*100) + '%';
+      if (colors) {
+          value.style.backgroundColor = window.probToColor(width*100);
+      }
+      bar.textContent = qualityName + ': ' + quality;
+      if (colors) {
+          bar.textContent += '/' + max;
+      }
+      bar.appendChild(value);
+      return bar;
+  };
+
+
+  window.dendryModifyUI = main;
+  console.log("Modifying stats: see dendryUI.dendryEngine.state.qualities");
+
+  window.onload = function() {
+    window.dendryUI.loadSettings();
+    window.pinnedCardsDescription = "Advisor cards - actions are only usable once per 6 months.";
+  };
+
+}());
