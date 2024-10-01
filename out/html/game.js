08eca3446b6c7b6090c74b5132d3fd6e7cb9e165
aucchen <cchen.intfic@gmail.com>
2024-09-30 22:48:35 -0700

    images

--- out/html/game.js
+++ out/html/game.js
@@ -19,8 +19,10 @@
   var TITLE = "Social Democracy: An Alternate History" + '_' + "Autumn Chen";
 
   // the url is a link to game.json
+  // test url: https://aucchen.github.io/social_democracy_mods/v0.1.json
   // TODO; 
   window.loadMod = function(url) {
+      ui.loadGame(url);
   };
 
   window.showStats = function() {
@@ -93,12 +95,22 @@
       window.dendryUI.saveSettings();
   };
 
+  window.enableImages = function() {
+      window.dendryUI.show_portraits = true;
+      window.dendryUI.saveSettings();
+  };
+
+  window.disableImages = function() {
+      window.dendryUI.show_portraits = false;
+      window.dendryUI.saveSettings();
+  };
 
   // populates the checkboxes in the options view
   window.populateOptions = function() {
     var disable_bg = window.dendryUI.disable_bg;
     var animate = window.dendryUI.animate;
     var disable_audio = window.dendryUI.disable_audio;
+    var show_portraits = window.dendryUI.show_portraits;
     if (disable_bg) {
         $('#backgrounds_no')[0].checked = true;
     } else {
@@ -113,6 +125,11 @@
         $('#audio_no')[0].checked = true;
     } else {
         $('#audio_yes')[0].checked = true;
+    }
+    if (show_portraits) {
+        $('#images_yes')[0].checked = true;
+    } else {
+        $('#images_no')[0].checked = true;
     }
   };
 
