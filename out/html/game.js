31c59629c1bc30fe3f1262659b588510aa4754fe
aucchen <cchen.intfic@gmail.com>
2024-10-06 14:34:15 -0700

    mod loader

--- out/html/game.js
+++ out/html/game.js
@@ -32,6 +32,15 @@
         window.dendryUI.dendryEngine.goToScene('library');
     }
   };
+
+  window.showMods = function() {
+    window.hideOptions();
+    if (window.dendryUI.dendryEngine.state.sceneId.startsWith('mod_loader')) {
+        window.dendryUI.dendryEngine.goToScene('backSpecialScene');
+    } else {
+        window.dendryUI.dendryEngine.goToScene('mod_loader');
+    }
+  };
   
   window.showOptions = function() {
       var save_element = document.getElementById('options');
