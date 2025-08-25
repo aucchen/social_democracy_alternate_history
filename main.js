9295149c46020659f45212625604937c3e7533bb
aucchen <cchen.intfic@gmail.com>
2025-08-24 22:21:07 -0700

    update deploy script

--- main.js
+++ main.js
@@ -7,17 +7,22 @@ const steamworks = require('steamworks.js');
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    autoHideMenuBar: true,
+    //width: 1600,
+    //height: 900,
+    //titleBarStyle: 'hidden',
+    //fullscreen: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      //contextIsolation: false,
-      //nodeIntegration: true
+      contextIsolation: false,
+      // this breaks jquery if it's used as a separate script, but not if it's done inline.
+      nodeIntegration: true
     }
-  })
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
+  mainWindow.maximize();
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -27,21 +32,21 @@ function createWindow () {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
