349cf85076d0b2613f14a66198e2e1a58dbd3b25
Araminty <aramintywhitesell@gmail.com>
2025-09-09 23:04:26 -0400

    including the function

--- out/html/game.js
+++ out/html/game.js
@@ -16,7 +16,7 @@
     // Add your custom code here.
   };
 
-  var TITLE = "Social Democracy: An Alternate History" + '_' + "Autumn Chen";
+  var TITLE = "Mikasa Democracy: A future history" + '_' + "Araminty Whitesell";
 
   // the url is a link to game.json
   // test url: https://aucchen.github.io/social_democracy_mods/v0.1.json
